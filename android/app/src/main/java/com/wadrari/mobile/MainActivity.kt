f083c5f03459373e1b703f9fb415b7781e01dd5c
Ilyas Ouhnine <ilyas.ouhnine@centrale-casablanca.ma>
2025-09-12 14:22:02 +0000

    Prebuild Android with Expo

--- android/app/src/main/java/com/wadrari/mobile/MainActivity.kt
+++ android/app/src/main/java/com/wadrari/mobile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wadrari.mobile
 
+import android.os.Build
 import android.os.Bundle
 
 import com.facebook.react.ReactActivity
@@ -7,28 +8,37 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import expo.modules.ReactActivityDelegateWrapper
+
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
-    super.onCreate(savedInstanceState)
+    super.onCreate(null)
   }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "wadrari-mobile"
+  override fun getMainComponentName(): String = "main"
 
-    /**
+  /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
-    return DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    return ReactActivityDelegateWrapper(
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
   }
-}
 
   /**
     * Align the back button behavior with Android S
